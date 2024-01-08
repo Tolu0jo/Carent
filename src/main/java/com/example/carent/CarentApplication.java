d02e9f80bdfd74c79becf67d6f18f59192eaf93c
toluOjo <ojotolulope137@gmail.com>
2024-01-08 09:55:37 +0100

    connect to database

---
+++ src/main/java/com/example/carent/CarentApplication.java
@@ -0,0 +1,13 @@
+package com.example.carent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/carent/CarentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
